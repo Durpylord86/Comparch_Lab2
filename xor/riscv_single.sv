a27810108e8683e2eb179cc3a7345bb9a7f83618
ericasaenz <158093293+ericasaenz@users.noreply.github.com>
2025-03-10 14:39:10 -0500

    Update riscv_single.sv

--- xor/riscv_single.sv
+++ xor/riscv_single.sv
@@ -132,12 +132,15 @@ module maindec (input  logic [6:0] op,
    always_comb
      case(op)
        // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump
-       7'b0000011: controls = 11'b1_00_1_0_01_0_00_0; // lw
-       7'b0100011: controls = 11'b0_01_1_1_00_0_00_0; // sw
-       7'b0110011: controls = 11'b1_xx_0_0_00_0_10_0; // R–type
-       7'b1100011: controls = 11'b0_10_0_0_00_1_01_0; // beq
-       7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; // I–type ALU
-       7'b1101111: controls = 11'b1_11_0_0_10_0_00_1; // jal
+       7'b0000011: controls = 11'b1_00_1_0_01_0_00_0; // lw and includes lb,lbu,lh,lhu?
+       7'b0100011: controls = 11'b0_01_1_1_00_0_00_0; // sw and should include both s -types
+       7'b0110011: controls = 11'b1_xx_0_0_00_0_10_0; // R–type and should include all r-types and xor????
+       7'b1100011: controls = 11'b0_10_0_0_00_1_01_0; // beq and should include all b types
+       7'b0010011: controls = 11'b1_00_1_0_00_0_10_0; // I–type ALU, slli,sltiu,srli
+       7'b1101111: controls = 11'b1_11_0_0_10_0_00_1; // jal I type
+       7'b0110111: controls = 11'b1_10_1_0_00_0_11_0; // lui (double-check) u type
+       7'b1100111: controls=  11'b1_00_1_0_00_0_10_0//jalr
+     //  7'b0000011: controls// I-type lb,lbu,lh,lhu
        default: controls = 11'bx_xx_x_x_xx_x_xx_x; // ???
      endcase // case (op)
    
